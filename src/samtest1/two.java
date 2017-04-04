d7170be86eb645a0a7a9ab0cb84465de6d3551cf
s <gopikamal638@gmail.com>
2017-04-05 03:09:36 +0530

    modifited the two program and git Change-Id: Ia6834b13f6c6aa345154611cfe4cb75f07cd5dd8 Signed-off-by: s <gopikamal638@gmail.com>

--- src/samtest1/two.java
+++ src/samtest1/two.java
@@ -5,6 +5,8 @@ public class two {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing on two");
+		System.out.println("testing on 2");
+		System.out.println("testing on 3");
 	}
 
 }
