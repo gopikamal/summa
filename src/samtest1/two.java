a357113213437c46d2e18b066284a3796804dae2
s <gopikamal638@gmail.com>
2017-04-05 03:16:52 +0530

    modified and add  push to upstream Change-Id: Ibdd9f058f025244e9b9417c92f2f81b268ea9647 Signed-off-by: s <gopikamal638@gmail.com>

--- src/samtest1/two.java
+++ src/samtest1/two.java
@@ -4,9 +4,11 @@ public class two {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("testing on two");
-		System.out.println("testing on 2");
-		System.out.println("testing on 3");
+		//System.out.println("testing on two");
+	//	System.out.println("testing on 2");
+		//System.out.println("testing on 3");
+		System.out.println("successfully underatnd testing");
+		
 	}
 
 }
